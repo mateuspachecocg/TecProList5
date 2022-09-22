94f1a388df86993ab7bd03871efe427b356efeac
Mateus Pacheco <mateuspacheco@alu.ufc.br>
2022-09-22 14:54:07 -0300

    Created Class Robot

---
+++ src/question07/Robot.java
@@ -0,0 +1,77 @@
+package question07;
+
+public class Robot {
+	private int px, py;
+	private int moveInN, moveInNE, moveInE, moveInSE, moveInS, moveInSW, moveInW, moveInNW;
+
+	public Robot(int x0, int y0) {
+		px = x0;
+		py = y0;
+		moveInN = 0;
+		moveInNE = 0;
+		moveInE = 0;
+		moveInSE = 0;
+		moveInS = 0;
+		moveInSW = 0;
+		moveInW = 0;
+		moveInNW = 0;
+	}
+
+	public Robot() {
+		px = 0;
+		py = 0;
+		moveInN = 0;
+		moveInNE = 0;
+		moveInE = 0;
+		moveInSE = 0;
+		moveInS = 0;
+		moveInSW = 0;
+		moveInW = 0;
+		moveInNW = 0;
+		
+	}
+
+	public void moveToNorth() {
+		py++;
+		moveInN++;
+	}
+
+	public void moveToNorthEast() {
+		px++;
+		py++;
+		moveInNE++;
+	}
+
+	public void moveToEast() {
+		px++;
+		moveInE++;
+	}
+
+	public void moveToSouthEast() {
+		px++;
+		py--;
+		moveInSE++;
+	}
+
+	public void moveToSouth() {
+		py--;
+		moveInS++;
+	}
+
+	public void moveToSouthWest() {
+		px--;
+		py--;
+		moveInSW++;
+	}
+
+	public void moveToWest() {
+		px--;
+		moveInW++;		
+	}
+
+	public void moveToNorthWest() {
+		px--;
+		py++;
+		moveInNW++;
+	}
+}
